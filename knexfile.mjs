4ec3fdb2a0a8112d7c28b5a5c6ef369d026d454f
Brandon Wallace <brandonmwallace97@gmail.com>
2026-04-11 21:58:47 -0700

    add knexfile

---
+++ knexfile.mjs
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+
+/**
+ * @type { import('knex').Knex.Config }
+ */
+const config = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.PGHOST,
+      port: Number(process.env.PGPORT) || 5432,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD || undefined,
+      database: process.env.PGDATABASE,
+    },
+    migrations: {
+      directory: './migrations',
+      extension: 'js',
+    },
+  },
+};
+
+export default config;
