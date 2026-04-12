700b3a5ed8d9e2d18d815865ecf212be30469f8a
Brandon Wallace <brandonmwallace97@gmail.com>
2026-04-11 22:18:56 -0700

    create user table migration

---
+++ migrations/20260412045852_create_users.cjs
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+    await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "pgcrypto"');
+
+    await knex.schema.createTable('users', (table) => {
+        table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+        table.string('username', 30).notNullable().unique();
+        table.string('email', 255).notNullable().unique();
+        table.string('password_hash', 255).notNullable();
+        table.string('display_name', 50).nullable();
+        table.timestamps(true, true);
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists('users');
+};
