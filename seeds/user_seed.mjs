5ef8bac49932ffcc0b63175005fab49165881b7d
Brandon Wallace <brandonmwallace97@gmail.com>
2026-04-14 09:07:34 -0700

    hash password for seed

--- seeds/user_seed.mjs
+++ seeds/user_seed.mjs
@@ -1,14 +1,17 @@
+import bcrypt from 'bcrypt';
 
 export const seed = async (knex) => {
   // Deletes ALL existing entries
   await knex('users').del();
+
+  const hashed = await bcrypt.hash('password', 12);
 
   // Inserts seed entries
   await knex('users').insert([
     {
       username: 'botc_admin',
       email: 'bwallace@bwmobilelabs.com',
-      password_hash: 'FIXMELATER',
+      password_hash: hashed,
       display_name: 'BOTC Admin'
     }
   ]);
