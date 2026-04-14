907b113dfd731470c1fceb895db87e0c9a4df0e0
Brandon Wallace <brandonmwallace97@gmail.com>
2026-04-13 20:54:54 -0700

    get all scripts and authors

--- src/routes/scripts.js
+++ src/routes/scripts.js
@@ -6,11 +6,15 @@ const router = Router();
 // List scripts WIP
 /**
  * Raw SQL = 
- * SELECT name, description, is_official FROM scripts;
+ * SELECT name, description, is_official, username AS author
+ * FROM scripts
+ * LEFT JOIN users ON users.id = scripts.owner_id;
  */
 router.get('/', async (req, res) => {
 	try {
-		const scripts = await db('scripts').select('name', 'description', 'is_official');
+		const scripts = await db('scripts')
+			.select('name', 'description', 'is_official', 'username as author')
+			.leftJoin('users', 'users.id', 'scripts.owner_id');
 		res.json(scripts);
 	} catch (err) {
 		console.log(err)
